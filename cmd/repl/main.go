c71656bb07f08d96052038b45436372011229f75
Abiria <abiria.dev@gmail.com>
2023-12-05 10:08:42 +0900

    feat: support REPL

---
+++ cmd/repl/main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+
+	"github.com/abiriadev/goggle/pkg/goggle"
+	"github.com/abiriadev/goggle/pkg/query"
+	"github.com/chzyer/readline"
+)
+
+func pathArg() string {
+	flag.Parse()
+	f := flag.Arg(0)
+
+	if f == "" {
+		panic("provide path to target package")
+	}
+
+	return f
+}
+
+func main() {
+	f := pathArg()
+
+	index, e := goggle.Parse(f)
+
+	if e != nil {
+		panic(e)
+	}
+
+	rl, e := readline.New("λ ")
+	if e != nil {
+		panic(e)
+	}
+	defer rl.Close()
+
+	for {
+		line, e := rl.Readline()
+		if e != nil {
+			break
+		}
+
+		// fmt.Println(line)
+
+		qs := line
+
+		qp, e := query.QueryParser()
+		if e != nil {
+			panic(e)
+		}
+
+		q, e := qp.ParseString("", qs)
+		if e != nil {
+			fmt.Println(e)
+		}
+
+		for _, fd := range index.Query(*q) {
+			fmt.Println(fd.Name, "->", fd.Ret)
+		}
+	}
+}
--- go.mod
+++ go.mod
@@ -4,7 +4,9 @@ go 1.21.4
 
 require (
 	github.com/alecthomas/participle/v2 v2.1.1 // indirect
+	github.com/chzyer/readline v1.5.1 // indirect
 	github.com/kr/pretty v0.3.1 // indirect
 	github.com/kr/text v0.2.0 // indirect
 	github.com/rogpeppe/go-internal v1.9.0 // indirect
+	golang.org/x/sys v0.0.0-20220310020820-b874c991c1a5 // indirect
 )
