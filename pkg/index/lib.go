295358322b54acdb0088c7ca124906fafaa2164b
Abiria <abiria.dev@gmail.com>
2023-12-05 17:35:20 +0900

    feat: use `DeepEqual` to check argument equality

--- pkg/index/lib.go
+++ pkg/index/lib.go
@@ -1,14 +1,19 @@
 package index
 
 import (
+	"fmt"
+	"reflect"
+
 	"github.com/abiriadev/goggle/pkg/query"
 )
 
 func (idx *Index) Query(q query.Query) []FuncDef {
 	rl := make([]FuncDef, 0)
 
+	fmt.Println(q.Args)
+
 	for _, fd := range idx.Items {
-		if q.Ret == fd.Ret {
+		if q.Ret == fd.Ret && reflect.DeepEqual(q.Args, fd.Args) {
 			rl = append(rl, fd)
 		}
 	}
