9d900edd3668cb980c19cfd03bdc2fef4bc969a1
Abiria <abiria.dev@gmail.com>
2023-12-05 13:58:10 +0900

    feat: support JSON marshalling

---
+++ pkg/index/marshal.go
@@ -0,0 +1,24 @@
+package index
+
+import (
+	"encoding/json"
+	"io"
+	"os"
+)
+
+func (index *Index) MarshalJson() ([]byte, error) {
+	return json.Marshal(index)
+}
+
+func (index *Index) MarshalJsonTo(w io.Writer) error {
+	return json.NewEncoder(w).Encode(index)
+}
+
+func (index *Index) save(filename string) error {
+	w, e := os.Create(filename)
+	if e != nil {
+		return e
+	}
+
+	return index.MarshalJsonTo(w)
+}
