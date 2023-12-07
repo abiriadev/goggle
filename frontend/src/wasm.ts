763b72dccca6d6fb82fe5404dc31e5757b753f63
Abiria <abiria.dev@gmail.com>
2023-12-08 06:58:20 +0900

    feat: load wasm

--- frontend/src/wasm.ts
+++ frontend/src/wasm.ts
@@ -4,7 +4,7 @@ import './syntaxck.d.ts'
 export const loadWasm = async (): Promise<void> => {
 	const goWasm = new window.Go()
 	const result = await WebAssembly.instantiateStreaming(
-		fetch('/syntaxck.wasm'),
+		fetch('/syntaxck.pack.wasm'),
 		goWasm.importObject,
 	)
 	goWasm.run(result.instance)
