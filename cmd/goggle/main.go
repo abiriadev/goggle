614bab2345c55125ff1831a7913930a0c4f39935
Abiria <abiria.dev@gmail.com>
2023-12-07 14:13:08 +0900

    feat: update goggle to serve http server

--- cmd/goggle/main.go
+++ cmd/goggle/main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
+	"log"
+	"log/slog"
+	"net/http"
+	"strconv"
 
-	"github.com/abiriadev/goggle/pkg/index"
-	"github.com/abiriadev/goggle/pkg/query"
+	"github.com/abiriadev/goggle/pkg/goggle"
 )
 
 func pathArg() string {
@@ -13,34 +17,70 @@ func pathArg() string {
 	f := flag.Arg(0)
 
 	if f == "" {
-		panic("provide path to target package")
+		return "index.gob"
 	}
 
 	return f
 }
 
+type QueryRequest struct {
+	Q string `json:"q"`
+}
+
+type QueryHandler struct {
+	Goggle goggle.Goggle
+}
+
+func (qh *QueryHandler) Query(query string) (goggle.ResultSet, error) {
+	return qh.Goggle.Query(query)
+}
+
+func (qh *QueryHandler) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
+	slog.Info("Request", "method", r.Method, "path", r.URL)
+
+	var req QueryRequest
+	err := json.NewDecoder(r.Body).Decode(&req)
+	if err != nil {
+		rw.WriteHeader(400)
+		rw.Write([]byte(err.Error()))
+		return
+	}
+
+	slog.Info("Query", "q", req.Q)
+
+	rs, err := qh.Query(req.Q)
+	if err != nil {
+		rw.WriteHeader(400)
+		rw.Write([]byte(err.Error()))
+	}
+
+	slog.Info("ResultSet", "items found", len(rs.Results))
+
+	err = rs.MarshalJsonTo(rw)
+	if err != nil {
+		rw.WriteHeader(500)
+		rw.Write([]byte(err.Error()))
+	}
+}
+
 func main() {
+	port := flag.Int("port", 6099, "port number to bind")
 	f := pathArg()
 
-	index, e := index.IndexPackage(f)
-
-	if e != nil {
-		panic(e)
+	goggle, err := goggle.NewGoggle(goggle.Config{Limit: 10})
+	if err != nil {
+		panic(err)
 	}
 
-	qs := "() bool"
-
-	qp, e := query.QueryParser()
-	if e != nil {
-		panic(e)
+	err = goggle.Load(f)
+	if err != nil {
+		panic(err)
 	}
 
-	q, e := qp.ParseString("", qs)
-	if e != nil {
-		panic(e)
-	}
+	qh := QueryHandler{Goggle: goggle}
 
-	for _, fd := range index.Query(*q) {
-		fmt.Println(fd.Name, "->", fd.Ret)
-	}
+	http.Handle("/search", &qh)
+
+	slog.Info("server is listening on", "port", strconv.Itoa(*port))
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), nil))
 }
