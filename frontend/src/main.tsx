ce4029255b41ffd2e80c680c3c8d06d6b59a5813
Abiria <abiria.dev@gmail.com>
2023-12-08 08:53:04 +0900

    feat: use `<ThemeProvider />`

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,11 +2,23 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import {
+	ThemeProvider,
+	createTheme,
+} from '@mui/material/styles'
+
+const theme = createTheme({
+	palette: {
+		mode: 'dark',
+	},
+})
 
 ReactDOM.createRoot(
 	document.getElementById('root')!,
 ).render(
 	<React.StrictMode>
-		<App />
+		<ThemeProvider theme={theme}>
+			<App />
+		</ThemeProvider>
 	</React.StrictMode>,
 )
