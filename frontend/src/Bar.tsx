b6f09a2a7e7bf27e3aeb77be8b0e8d4dfd416f51
Abiria <abiria.dev@gmail.com>
2023-12-08 15:13:56 +0900

    feat: add link

--- frontend/src/Bar.tsx
+++ frontend/src/Bar.tsx
@@ -1,4 +1,9 @@
-import { AppBar, Toolbar, Typography } from '@mui/material'
+import {
+	AppBar,
+	Link,
+	Toolbar,
+	Typography,
+} from '@mui/material'
 import GitHubIcon from '@mui/icons-material/GitHub'
 
 export const Bar = () => (
@@ -8,8 +13,14 @@ export const Bar = () => (
 		color="primary"
 	>
 		<Toolbar variant="dense">
-			<Typography flexGrow={1}>News</Typography>
-			<GitHubIcon />
+			<Typography flexGrow={1}>Goggle</Typography>
+			<Link
+				href="https://github.com/abiriadev/goggle"
+				color="inherit"
+				underline="none"
+			>
+				<GitHubIcon />
+			</Link>
 		</Toolbar>
 	</AppBar>
 )
