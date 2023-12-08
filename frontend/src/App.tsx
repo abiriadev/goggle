32c7770bef3aa47a1e0a0b1f2595056e5cea25be
Abiria <abiria.dev@gmail.com>
2023-12-08 11:38:25 +0900

    refactor: componentize `<Search />`

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,44 +1,10 @@
-import { useEffect, useState } from 'react'
 import './App.css'
-import { loadWasm } from './wasm'
-import {
-	Autocomplete,
-	Link,
-	TextField,
-	Typography,
-	useTheme,
-} from '@mui/material'
-import './syntaxck.d.ts'
-import { ResultItem, query } from './goggle.ts'
+import { useTheme } from '@mui/material'
 import { Bar } from './Bar.tsx'
+import { Search } from './Search.tsx'
 
 function App() {
-	const [wasmLoad, setWasmLoad] = useState(false)
-	const [resultSet, setResultSet] = useState<
-		Array<ResultItem>
-	>([])
-	const [inp, setInp] = useState('')
 	const theme = useTheme()
-
-	useEffect(() => {
-		;(async () => {
-			await loadWasm()
-			setWasmLoad(true)
-		})()
-	}, [])
-
-	useEffect(() => {
-		if (!(wasmLoad && window.syntaxck(inp))) return
-		;(async () => {
-			try {
-				const rs = await query(inp)
-
-				setResultSet(rs.items)
-			} catch (e) {
-				console.error(e)
-			}
-		})()
-	}, [inp])
 
 	return (
 		<>
@@ -51,83 +17,7 @@ function App() {
 				>
 					Goggle
 				</h1>
-				<Autocomplete
-					freeSolo
-					sx={{
-						width: 600,
-					}}
-					options={resultSet}
-					filterOptions={_ => _}
-					getOptionLabel={o =>
-						typeof o === 'string' ? o : o.sig
-					}
-					renderInput={p => (
-						<TextField
-							sx={{
-								'& .MuiOutlinedInput-root':
-									{
-										'& > fieldset': {
-											borderColor:
-												theme
-													.palette
-													.primary
-													.main,
-										},
-									},
-								'& .MuiOutlinedInput-root:hover':
-									{
-										'& > fieldset': {
-											borderColor:
-												theme
-													.palette
-													.primary
-													.main,
-										},
-									},
-							}}
-							{...p}
-						/>
-					)}
-					renderOption={(p, o) => (
-						<li {...p}>
-							<Link
-								href={o.link}
-								underline="none"
-								target="_blank"
-							>
-								{o.sig}
-								<Typography
-									paragraph
-									noWrap
-									variant="caption"
-									color="#bbbbbb"
-									sx={{
-										fontWeight: 300,
-									}}
-								>
-									{o.summary}
-								</Typography>
-							</Link>
-						</li>
-					)}
-					onInputChange={(_, i) => setInp(i)}
-					onChange={(_, o, r) => {
-						if (r === 'selectOption') {
-							window.open(
-								typeof o === 'string'
-									? o
-									: o?.link,
-								'_blank',
-							)
-						}
-						console.log(
-							'value:',
-							o,
-							'reason:',
-							r,
-						)
-					}}
-				/>
+				<Search />
 			</main>
 		</>
 	)
---
+++ frontend/src/Search.tsx
@@ -0,0 +1,108 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+import { loadWasm } from './wasm'
+import {
+	Autocomplete,
+	Link,
+	TextField,
+	Typography,
+	useTheme,
+} from '@mui/material'
+import './syntaxck.d.ts'
+import { ResultItem, query } from './goggle.ts'
+
+export const Search = () => {
+	const theme = useTheme()
+	const [wasmLoad, setWasmLoad] = useState(false)
+	const [resultSet, setResultSet] = useState<
+		Array<ResultItem>
+	>([])
+	const [inp, setInp] = useState('')
+
+	useEffect(() => {
+		;(async () => {
+			await loadWasm()
+			setWasmLoad(true)
+		})()
+	}, [])
+
+	useEffect(() => {
+		if (!(wasmLoad && window.syntaxck(inp))) return
+		;(async () => {
+			try {
+				const rs = await query(inp)
+
+				setResultSet(rs.items)
+			} catch (e) {
+				console.error(e)
+			}
+		})()
+	}, [inp])
+
+	return (
+		<Autocomplete
+			freeSolo
+			sx={{
+				width: 600,
+			}}
+			options={resultSet}
+			filterOptions={_ => _}
+			getOptionLabel={o =>
+				typeof o === 'string' ? o : o.sig
+			}
+			renderInput={p => (
+				<TextField
+					sx={{
+						'& .MuiOutlinedInput-root': {
+							'& > fieldset': {
+								borderColor:
+									theme.palette.primary
+										.main,
+							},
+						},
+						'& .MuiOutlinedInput-root:hover': {
+							'& > fieldset': {
+								borderColor:
+									theme.palette.primary
+										.main,
+							},
+						},
+					}}
+					{...p}
+				/>
+			)}
+			renderOption={(p, o) => (
+				<li {...p}>
+					<Link
+						href={o.link}
+						underline="none"
+						target="_blank"
+					>
+						{o.sig}
+						<Typography
+							paragraph
+							noWrap
+							variant="caption"
+							color="#bbbbbb"
+							sx={{
+								fontWeight: 300,
+							}}
+						>
+							{o.summary}
+						</Typography>
+					</Link>
+				</li>
+			)}
+			onInputChange={(_, i) => setInp(i)}
+			onChange={(_, o, r) => {
+				if (r === 'selectOption') {
+					window.open(
+						typeof o === 'string' ? o : o?.link,
+						'_blank',
+					)
+				}
+				console.log('value:', o, 'reason:', r)
+			}}
+		/>
+	)
+}
