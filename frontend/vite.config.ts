1b8db7f9b0c835cf164ad358e11ad5170f00cfea
Abiria <abiria.dev@gmail.com>
2023-12-12 10:13:16 +0900

    fix: declare `*.wasm` module so that tsc will compile

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig(({ mode }) => ({
 	plugins: [react()],
 
-	base: mode === 'development' ? './' : 'goggle',
+	base: mode === 'development' ? '/' : '/goggle',
 
 	server: {
 		proxy: {
